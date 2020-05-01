362c56f9930ebd2b9694ca16dd2e36ac04143dd9
QiWeiLong <291532919@qq.com>
2020-05-01 15:27:35 +0800

    第二次提交，前端代码提交测试

---
+++ src/utils/api.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { Message } from 'element-ui';
+
+axios.interceptors.response.use(success=>{
+    if(success.status && success.status == 200 && success.data.status == 500 ){
+        Message.error({message:success.data.msg})
+        return;
+    }
+    return success.data;
+},error => {
+    if(err.response.status == 504 || err.response.status == 404 ){
+        Message.error({message:'服务器被吃了'})
+    }else if(err.response.status == 403){
+        Message.error({message:'权限不足请联系管理员'})
+    }else if(err.response.status == 401){
+        Message.error({message:'尚未登陆，请登录'})
+    }else{
+        if(error.response.data.msg){
+            Message.error({message:error.response.data.msg})
+        }else{
+            Message.error({message:'未知错误'})
+        }
+    }
+    return;
+})
+
+let base ='';
+
+export const postKeyValueRequest=(url,params)=>{
+    return axios({
+       method:'post',
+        url:`${base}${url}`,
+        data:params,
+        transformRequest:[function(data){
+           let ret = '';
+           for(let i in data){
+               ret+=encodeURIComponent(i)+'='+encodeURIComponent(data[i])+'&'
+           }
+           console.log(ret);
+           return ret;
+        }],
+        headers:{
+           'Content-Type':'application/x-www-form-urlencoded'
+        }
+    });
+}
---
+++ src/views/Login.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <!-- 验证规则rules,对象loginForm,太丑了写个style .loginContainer-->
+        <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
+            <h3 class="loginTitle">系统登陆</h3>
+            <!-- prop数据传递-->
+            <el-form-item prop="username">
+                <!-- auto-complete：自动补全 placeholder:占位符-->
+                <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <!-- auto-complete：自动补全 placeholder:占位符-->
+                <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <!-- 记住账号密码-->
+            <el-checkbox v-model="checked">记住账号密码</el-checkbox>
+            <el-button type="primary" style="width: 100%" @click="submitLogin">登录</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    import {postKeyValueRequest} from "../utils/api";
+
+    export default {
+        name: "Login",
+        data(){
+            return{
+                loginForm:{
+                    username:'admin',
+                    password:'123'
+                },
+                checked:true,
+                rules:{
+                    username:[{required:true,message:'请输入用户名',trigger:'blur'}],
+                    password:[{required:true,message:'请输入密码',trigger:'blur'}],
+                }
+            }
+        },
+        methods:{
+            submitLogin(){
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        postKeyValueRequest('/doLogin',this.loginForm).then(resp=>{
+                            if(resp){
+                                window.sessionStorage.setItem("user",JSON.stringify(resp.obj));
+                                this.$router.replace('/home');
+                            }
+                        })
+                    } else {
+                        this.$message.error('请输入所有字段');
+                        return false;
+                    }
+                });
+            },
+        }
+    }
+</script>
+
+<style>
+    .loginTitle{
+     margin: 20px auto 40px auto;
+     text-align: center;
+    }
+    .loginContainer{
+        border-radius: 15px;
+        background-clip: padding-box;
+        margin: 180px auto;
+        width: 350px;
+        padding:25px;
+        background: #ffffff;
+        border: 1px solid burlywood;
+        box-shadow: 0 0 25px #cac6c6;
+    }
+</style>
